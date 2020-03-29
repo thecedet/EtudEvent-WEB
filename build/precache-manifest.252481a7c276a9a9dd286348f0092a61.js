8c994a102a87b656e3677c8522d7d3a6498bed44
thecedet <thecedet@outlook.fr>
2020-03-29 19:09:56 +0200

    change gitignore

---
+++ build/precache-manifest.252481a7c276a9a9dd286348f0092a61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1831ca7d3f53e3af243017afe56cbc71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8699796b33daae83f642",
+    "url": "/static/css/main.9b4f5a50.chunk.css"
+  },
+  {
+    "revision": "6c51add276416f4b6e63",
+    "url": "/static/js/2.fabd6fb7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fabd6fb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8699796b33daae83f642",
+    "url": "/static/js/main.85c2aca8.chunk.js"
+  },
+  {
+    "revision": "caf2a04453df4bace4e4",
+    "url": "/static/js/runtime-main.009173a0.js"
+  }
+]);
