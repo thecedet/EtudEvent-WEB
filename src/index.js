0460fc778163f68624077bc009e2a43746da68ff
thecedet <thecedet@outlook.fr>
2020-03-29 19:25:11 +0200

    add 404 page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route,Switch } from 'react-router-dom';
 
 import "./index.css"
 
@@ -9,14 +9,17 @@ import Register from './screens/auth/register';
 import AnnonceMain from "./screens/annonce/"
 import Annonce from "./screens/annonce/annonce"
 
+import NotFound from "./screens/NotFound"
+
 render(
   <Router>
-    <div>
+    <Switch>
       <Route exact path="/" component={AnnonceMain} />
       <Route exact path="/login" component={Login} />
       <Route exact path="/register" component={Register} />
       <Route exact path="/annonce/:uid" component={Annonce} />
-    </div>
+      <Route component={NotFound}/>
+    </Switch>
   </Router>,
   document.getElementById('root')
 );
